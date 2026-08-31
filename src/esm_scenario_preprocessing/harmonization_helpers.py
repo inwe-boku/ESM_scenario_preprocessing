63db02063da6c339d598ee6f553ad951e424fe9d
max_nutz <max.nutz@boku.ac.at>
2026-08-31 12:29:28 +0200

    Revert "9 compute energy input per industry output from nefi scenarios"

--- src/esm_scenario_preprocessing/harmonization_helpers.py
+++ src/esm_scenario_preprocessing/harmonization_helpers.py
@@ -3,27 +3,15 @@
 import pandas as pd
 
 nefi_to_pypsa_inputs_dict: dict[str, list[str]] = {
-    "elec": ["Electricity", "Electricity for HPs", "El. for Heat Pumps", "El. for H2"],
+    "elec": ["Electricity", "Electricity for HPs"],
     "coal and coke": ["Coal"],  # combines coal and coke in pypsa-at
     "biomass": ["Biofuels"],
-    "methane": [
-        "Gas",
-        "Biogas",
-        "Synth-CH4",
-        "Gas grid mix",
-        "Natural gas, Bio-CH4 and Syn-CH4",
-        "Bio-CH4",
-        "CH4",
-        "Gas grid ix",
-    ],
-    "hydrogen": ["Hydrogen, H2", "Hydrogen"],
+    "methane": ["Gas", "Biogas", "Synth-CH4"],
+    "hydrogen": ["Hydrogen, H2"],
     "heat": ["District Heating", "Solar thermal"],
     "naphtha": ["Oil"],
     "ammonia": [],
     "methanol": [],
-    "waste": ["Waste"],
-    "losses": ["Transformation losses H2"],
-    "Total": ["Total", "Total Energy", "Total energy"],
 }
 
 pypsa_combinations_for_nefi_inputs_dict: dict[str, list[str]] = {
@@ -50,10 +38,7 @@ def energy_inputs_harmonization(df_in: pd.DataFrame) -> pd.DataFrame:
 
     nefi_fe_t = df_in.T
     for _pypsa_material, _nefi_material in nefi_to_pypsa_inputs_dict.items():
-        _available = [col for col in _nefi_material if col in nefi_fe_t.columns]
-        nefi_fe_t[_pypsa_material] = nefi_fe_t[_available].sum(
-            axis=1, numeric_only=False
-        )
-        nefi_fe_t = nefi_fe_t.drop(_available, axis=1)
+        nefi_fe_t[_pypsa_material] = nefi_fe_t[_nefi_material].sum(axis=1, numeric_only=False)
+        nefi_fe_t = nefi_fe_t.drop(_nefi_material, axis=1)
     df_out = nefi_fe_t.T
     return df_out
